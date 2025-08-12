839c8cec6be8b346062ee42b1cc136fa17638ce3
Alexandr <predigeralexndr@gmail,com>
2025-08-12 09:33:44 +0400

    add new task

---
+++ LeetCodeTasks/TwoPointers/Program.cs
@@ -0,0 +1,5 @@
+﻿using TwoPointers;
+//Container With Most Water - суть задачи, что кадждый индекс массива это высота границы для воды, нужно найти максимально большую площадь покрытия воды в пределах массива,
+var array = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+var result1 = TwoPointerImplementation.Execute(array);
+Console.ReadKey();
---
+++ LeetCodeTasks/TwoPointers/TwoPointerImplementation.cs
@@ -0,0 +1,36 @@
+﻿namespace TwoPointers;
+
+public static class TwoPointerImplementation
+{
+    public static int Execute(int[] height) {
+        var left = 0; 
+        var right = height.Length-1;
+        var maxArea = 0;
+        while(left<right){
+            var area = (right-left)*Min(height[left],height[right]);
+            if (area > maxArea)
+            {
+                maxArea = area;
+            }
+
+            if (height[right] > height[left])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static int Min(int firstNumber, int secondNumber){
+        if(firstNumber<secondNumber){
+            return firstNumber;
+        }
+
+        return secondNumber;
+    }
+}
