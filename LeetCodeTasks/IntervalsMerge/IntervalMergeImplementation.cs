707573d7e87fcb570cb4a94468b7af11dc7af983
Alexandr <predigeralexndr@gmail,com>
2025-08-12 23:37:41 +0400

    Add Interval Merge project

---
+++ LeetCodeTasks/IntervalsMerge/IntervalMergeImplementation.cs
@@ -0,0 +1,43 @@
+﻿namespace IntervalsMerge;
+
+public static class IntervalMergeImplementation
+{
+    public static int[][] Execute(int[][] intervals, int[] newInterval)
+    {
+        if (intervals.Length == 0)
+        {
+            return new int[][] { newInterval };
+        }
+
+        var left = newInterval[0];
+        var right = newInterval[1];
+
+        var result = new List<int[]>();
+
+        var i = 0;
+
+        while (i < intervals.Length && newInterval[0] > intervals[i][1])
+        {
+            result.Add(intervals[i]);
+            i++;
+        }
+
+        while (i < intervals.Length && newInterval[1] >= intervals[i][0])
+        {
+            left = Math.Min(intervals[i][0], left);
+            right = Math.Max(intervals[i][1], right);
+
+            i++;
+        }
+
+        result.Add(new[] { left, right });
+
+        while (i < intervals.Length)
+        {
+            result.Add(intervals[i]);
+            i++;
+        }
+
+        return result.ToArray();
+    }
+}
---
+++ LeetCodeTasks/IntervalsMerge/Program.cs
@@ -0,0 +1,9 @@
+﻿//57. Insert Interval - суть задачи - есть отсортированный список  непересекающихся интервалов. На вход подаётся новый
+//интервал и его нужно вставить в этот массив, чтобы он не перескался с другими, если он будет пересекаться, то объединить его с пересекающимся интервалами 
+
+using IntervalsMerge;
+
+var intervals = new[] { new[] { 1, 3 }, new[]{ 6, 9 } };
+var newInterval = new[] {2, 5};
+var result1 = IntervalMergeImplementation.Execute(intervals, newInterval);
+Console.ReadKey();
