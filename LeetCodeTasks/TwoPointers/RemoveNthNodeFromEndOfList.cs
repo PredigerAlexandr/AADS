9304bb5c31d4d1511dfc4710494fd886ff20e59d
Alexandr <predigeralexndr@gmail.com>
2025-08-14 22:25:36 +0400

    add TwoPointers solution

---
+++ LeetCodeTasks/TwoPointers/RemoveNthNodeFromEndOfList.cs
@@ -0,0 +1,53 @@
+﻿namespace TwoPointers;
+
+public static class RemoveNthNodeFromEndOfList
+{
+    public static ListNode Execute(ListNode head, int n)
+    {
+        if (head.next == null)
+        {
+            return null;
+        }
+
+        var currentNode = head;
+        var count = 0;
+        var globalCount = 1;
+
+        ListNode preNNode = head;
+        while (currentNode.next != null)
+        {
+            globalCount++;
+            if (count < n)
+            {
+                count++;
+            }
+            else
+            {
+                preNNode = preNNode.next;
+            }
+
+            currentNode = currentNode.next;
+        }
+
+        if (globalCount == n)
+        {
+            return head.next;
+        }
+
+        preNNode.next = preNNode.next.next;
+
+        return head;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
